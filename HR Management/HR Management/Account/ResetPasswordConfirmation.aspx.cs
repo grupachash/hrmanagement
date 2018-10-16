834f1d6b2289f3c77a14464b512771cf70c294f0
dawidektr <dawdziedziak@gmail.com>
2018-10-16 20:06:49 +0200

    Initial

---
+++ HR Management/HR Management/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace HR_Management.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ HR Management/HR Management/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HR_Management.Startup))]
+namespace HR_Management
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
