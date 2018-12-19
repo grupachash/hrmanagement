0c41d239987a2921d331529ce89dcc2d8cef7808
dawidektr <dawdziedziak@gmail.com>
2018-12-19 19:21:27 +0100

    Added database file

---
+++ HR Management/HR_db.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.
+using System.Data.SqlClient;
+public class HR_db
+{   
+	public HR_db()
+	{
+        System.Data.SqlClient.SqlConnection con = new System.Data.SqlClient.SqlConnection();
+       con.
+
+	}
+}
